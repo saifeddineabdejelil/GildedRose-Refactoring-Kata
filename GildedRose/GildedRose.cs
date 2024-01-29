e4c3424d85452da8ae6234cde662406dc71235c8
Saif Eddine Abdejelil <saifeddineabdejelil@gmail.com>
2024-01-29 11:11:32 +0100

    Cleanup if conditions remove redundant condition and merge others

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -16,12 +16,9 @@ namespace csharp
             {
                 if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (item.Quality > 0)
+                    if (item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
                     {
-                        if (item.Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            item.Quality = item.Quality - 1;
-                        }
+                        item.Quality = item.Quality - 1;
                     }
                 }
                 else
@@ -34,18 +31,13 @@ namespace csharp
                         {
                             if (item.SellIn < 11)
                             {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
+                                item.Quality = item.Quality + 1;
                             }
 
                             if (item.SellIn < 6)
                             {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
+                                item.Quality = item.Quality + 1;
+
                             }
                         }
                     }
@@ -62,12 +54,9 @@ namespace csharp
                     {
                         if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
                         {
-                            if (item.Quality > 0)
+                            if (item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
                             {
-                                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    item.Quality = item.Quality - 1;
-                                }
+                                item.Quality = item.Quality - 1;
                             }
                         }
                         else
--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Let's try to do refactoring for GildedRose-Refactoring-Kata in c#
 2- Push the original code in c# in the new repository.
 3- Create new branch called  refactGildedRose to work on it.
 4- Based on GildedRose Requirements Specification file : we create unit tests which test all cases. All tests passed ( green) with actual code. This step in very important to start refactoring after any change of code we will re run all tests and  they shoud all passed.
-5- Start refactoring of GildedRose class : add foreach to replace for and loop in element not with index : avoid using index when we call item
+5- Start refactoring of GildedRose class : add foreach to replace for and loop in element not with index : avoid using index when we call item.
+6- Cleanup if conditions : remove redudant condition and merge child if (without else ) with parent if.
