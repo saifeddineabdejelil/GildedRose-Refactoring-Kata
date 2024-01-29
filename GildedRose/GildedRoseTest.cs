e0d9b7249e6875985e090bd5aeb6393bf5b46ec2
Saif Eddine Abdejelil <saifeddineabdejelil@gmail.com>
2024-01-29 08:59:17 +0100

    Create unit tests for all cases based on GildedRose specification

--- GildedRose/GildedRoseTest.cs
+++ GildedRose/GildedRoseTest.cs
@@ -6,13 +6,174 @@ namespace csharp
     [TestFixture]
     public class GildedRoseTest
     {
-        [Test]
-        public void foo()
+        // to get list of items ( with 1 element ) from an item details
+        private static List<Item> CreateItems(string name, int sellin, int quality)
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.AreEqual("fixme", Items[0].Name);
+            var items = new List<Item>();
+            return new List<Item> { new Item { Name = name, SellIn = sellin, Quality = quality } };
+
         }
+        // create gildedRose object from list of items
+        private static GildedRose GetGildedRose(List<Item> listItems)
+        {
+            return new GildedRose(listItems);
+        }
+        [Test]
+        public void Test_ItemNameWhenCreatingListAndGildedRoseObject()
+        {
+            var Items = CreateItems("foo", 0, 0);
+            GildedRose gildedRose = GetGildedRose(Items);
+            gildedRose.UpdateQuality();
+
+            // name shoud be foo
+            Assert.AreEqual("foo", Items[0].Name);
+        }
+
+        [Test]
+        public void Test_UpdateSellIn_GenericItem()
+        {
+            // create any item with sellin 7 and quality 7 
+            var Items = CreateItems("Generic", 7, 7);
+            GildedRose gildedRose = GetGildedRose(Items);
+            gildedRose.UpdateQuality();
+
+            // sellin should be reduced by 1 => 6
+            Assert.AreEqual(6, Items[0].SellIn);
+        }
+
+        [Test]
+        public void Test_UpdateQuality_GenericItem_QualityReducedBy1()
+        {
+            // create generic item with sellin 7 and quality 7 
+            var Items = CreateItems("Generic", 7, 7);
+            GildedRose gildedRose = GetGildedRose(Items);
+            gildedRose.UpdateQuality();
+
+            // quality should be reduced by 1 => 6
+            Assert.AreEqual(6, Items[0].Quality);
+        }
+
+        [Test]
+        public void Test_UpdateQuality_GenericItem_SellinLessOrEqualTo0QualityReducedBy2()
+        {
+            // create generic item the sell by date has passed (sellin 0) and quality 7 
+            var Items = CreateItems("Generic", 0, 7);
+            GildedRose gildedRose = GetGildedRose(Items);
+            gildedRose.UpdateQuality();
+
+            // quality should be reduced by 2 =>5
+            Assert.AreEqual(5, Items[0].Quality);
+        }
+
+        [Test]
+        public void Test_UpdateQuality_GenericItem_QualityShouldNotBeLessThan0()
+        {
+            // create generic item with sellin 7 and quality 0 
+            var Items = CreateItems("Generic", 7, 0);
+            GildedRose gildedRose = GetGildedRose(Items);
+            gildedRose.UpdateQuality();
+
+            // quality should not be reduced by 1 stay 0
+            Assert.AreEqual(0, Items[0].Quality);
+        }
+
+        [Test]
+        public void Test_UpdateQuality_AgedBrieItem_QualityShouldNotBeMorehan50()
+        {
+            // create Aged Brie item with quality 50 and sell in 6 days
+            var Items = CreateItems("Aged Brie", 6, 50);
+            GildedRose gildedRose = GetGildedRose(Items);
+            gildedRose.UpdateQuality();
+
+            // quality should not increase because it is 50
+            Assert.AreEqual(50, Items[0].Quality);
+        }
+
+        [Test]
+        public void Test_UpdateQuality_ZeroSellinAndQualityShouldNotBeNegative()
+        {
+            // create generic item with quality 0
+            var Items = CreateItems("Generic", 1, 0);
+            GildedRose gildedRose = GetGildedRose(Items);
+            gildedRose.UpdateQuality();
+
+            // quality should not be negative => stay 0
+            Assert.AreEqual(0, Items[0].Quality);
+        }
+
+        [Test]
+        public void Test_UpdateQuality_AgedBrieIncreaseQuality()
+        {
+            // create Aged Brie item with quality 6 and sell in 6 days
+            var Items = CreateItems("Aged Brie", 6, 6);
+            GildedRose gildedRose = GetGildedRose(Items);
+            gildedRose.UpdateQuality();
+
+            // quality should increase because it is Aged brie => 7
+            Assert.AreEqual(7, Items[0].Quality);
+        }
+
+        [Test]
+        public void Test_UpdateQuality_SulfurasKeepSameQuality()
+        {
+            // create surfurace item with quality 15 and sell in 6 days
+            var sulfurasItems = CreateItems("Sulfuras, Hand of Ragnaros", 6, 15);
+            GildedRose gildedRose = GetGildedRose(sulfurasItems);
+            gildedRose.UpdateQuality();
+
+            // quality surfurace sould not be reduced stay 15
+            Assert.AreEqual(15, sulfurasItems[0].Quality);
+        }
+
+
+        [Test]
+        public void Test_UpdateQuality_BackstageIncreaseQualityByOneWhenSellinMoreThan10()
+        {
+            // create Backstage item with quality 15 and sell in 12 days
+            var backstageItems = CreateItems("Backstage passes to a TAFKAL80ETC concert", 12, 15);
+            GildedRose gildedRose = GetGildedRose(backstageItems);
+            gildedRose.UpdateQuality();
+
+            // Backstage item : sellin > 10 quality should be increased by 1 => 16
+            Assert.AreEqual(16, backstageItems[0].Quality);
+        }
+
+
+        [Test]
+        public void Test_UpdateQuality_BackstageIncreaseQualityByTwoWhenSellinLessOrEqualThan10AndMoreThan5()
+        {
+            // create Backstage item with quality 15 and sell in 10 days
+            var backstageItems = CreateItems("Backstage passes to a TAFKAL80ETC concert", 10, 15);
+            GildedRose gildedRose = GetGildedRose(backstageItems);
+            gildedRose.UpdateQuality();
+
+            // Backstage item : sellin <= 10 quality should be increased by 2 => 17
+            Assert.AreEqual(17, backstageItems[0].Quality);
+        }
+        [Test]
+        public void Test_UpdateQuality_BackstageIncreaseQualityByTwoWhenSellinLessOrEqualThan5()
+        {
+            // create Backstage item with quality 15 and sell in 5 days
+            var backstageItems = CreateItems("Backstage passes to a TAFKAL80ETC concert", 5, 15);
+            GildedRose gildedRose = GetGildedRose(backstageItems);
+            gildedRose.UpdateQuality();
+
+            // Backstage item : sellin <= 5 quality should be increased by 3 => 18
+            Assert.AreEqual(18, backstageItems[0].Quality);
+        }
+
+        [Test]
+        public void Test_UpdateQuality_BackstageSetQuality0WhenSellinLessOrEqualTo0()
+        {
+            // create Backstage item with quality 15 and sell in 0 days
+            var backstageItems = CreateItems("Backstage passes to a TAFKAL80ETC concert", 0, 15);
+            GildedRose gildedRose = GetGildedRose(backstageItems);
+            gildedRose.UpdateQuality();
+
+
+            //Backstage Quality drops to 0 after the concert 
+            Assert.AreEqual(0, backstageItems[0].Quality);
+        }
+
     }
 }
--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Let's try to do refactoring for GildedRose-Refactoring-Kata in c#
 
 1-to copy of c# file from GildedRose-Refactoring-Kata repository.
 2- Push the original code in c# in the new repository.
+3- Based on GildedRose Requirements Specification file : we create unit tests which test all cases. All tests passed ( green) with actual code. This step in very important to start refactoring after any change of code we will re run all tests and  they shoud all passed.
